13b579b4151dd86bb97e9bbb83b61103bfcc1099
elmarguevarra <64261171+elmarguevarra@users.noreply.github.com>
2021-10-12 16:22:42 +0800

    Enhanced Test

--- GameOfLife_Test/Test.cs
+++ GameOfLife_Test/Test.cs
@@ -12,10 +12,12 @@ namespace GameOfLife_Test
     {
         public static void Main(string[] args)
         {
-            TestGameOfLife();
+            Test_InputMatrix_ShouldbeSamewith_OutputMatrix();
+            Test_OutputMatrix_ShouldPass_inIteration2_withRemainingLife();
+            Test_OutputMatrix_ShouldPass_inIteration10_withNoRemainingLife();
         }
 
-        public static void TestGameOfLife()
+        public static void Test_InputMatrix_ShouldbeSamewith_OutputMatrix()
         {
             var inputMatrix = new int[6, 6] {
                 { 0, 0, 0, 0, 0, 0 },
@@ -38,20 +40,102 @@ namespace GameOfLife_Test
             Assert(actualResults, mockResult);
         }
 
+        public static void Test_OutputMatrix_ShouldPass_inIteration2_withRemainingLife()
+        {
+            var inputMatrix = new int[,] {
+                { 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 1, 1, 0 },
+                { 0, 0, 1, 0, 1, 0 },
+                { 0, 0, 0, 1, 1, 0 },
+                { 0, 0, 0, 1, 0, 0 },
+                { 0, 0, 0, 0, 0, 0 }
+            };
+
+
+
+            var gameOfLifeObj = new GameOfLife()
+            {
+                iterations = 2,
+                matrix = inputMatrix
+            };
+
+            var actualResults = Program.StartGameOfLife(gameOfLifeObj);
+
+            var mockResult = new int[,]
+                {
+                    { 0, 0, 0, 0, 0, 0 },
+                    { 0, 0, 0, 1, 1, 0 },
+                    { 0, 0, 1, 0, 0, 1 },
+                    { 0, 0, 1, 0, 1, 1 },
+                    { 0, 0, 0, 1, 1, 0 },
+                    { 0, 0, 0, 0, 0, 0 },
+                };
+
+            Assert(actualResults, mockResult);
+        }
+
+        public static void Test_OutputMatrix_ShouldPass_inIteration10_withNoRemainingLife()
+        {
+            var inputMatrix = new int[,] {
+                { 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 1, 1, 0 },
+                { 0, 0, 1, 1, 0, 0 },
+                { 0, 1, 1, 1, 0, 0 },
+                { 0, 0, 0, 1, 0, 0 },
+                { 0, 0, 0, 0, 0, 0 }
+            };
+
+
+
+            var gameOfLifeObj = new GameOfLife()
+            {
+                iterations = 10,
+                matrix = inputMatrix
+            };
+
+            var actualResults = Program.StartGameOfLife(gameOfLifeObj);
+
+            var mockResult = new int[,]
+                {
+                    { 0, 0, 0, 0, 0, 0 },
+                    { 0, 0, 0, 0, 0, 0 },
+                    { 0, 0, 0, 0, 0, 0 },
+                    { 0, 0, 0, 0, 0, 0 },
+                    { 0, 0, 0, 0, 0, 0 },
+                    { 0, 0, 0, 0, 0, 0 },
+                };
+
+            Assert(actualResults, mockResult);
+        }
+
         private static void Assert(int[,] results, int[,] mockResult)
         {
             int xLimit = results.GetUpperBound(0), yLimit = results.GetUpperBound(1);
 
+            var match = true;
             for (int xctr = 0; xctr <= xLimit; xctr++)
             {
                 for (int yctr = 0; yctr <= yLimit; yctr++)
                 {
                     if (!results[xctr, yctr].Equals(mockResult[xctr, yctr]))
                     {
-                        Console.WriteLine("FAIL");
+                        match = false;
                         break;
                     }
                 }
+            }
+
+            if (match)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("PASS", Console.ForegroundColor);
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("FAIL", Console.ForegroundColor);
+                Console.ForegroundColor = ConsoleColor.Gray;
             }
         }
     }
