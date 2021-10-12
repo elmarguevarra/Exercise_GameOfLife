81b25da205237c2cc7d3e07b7a4d47d88922f27e
elmarguevarra <64261171+elmarguevarra@users.noreply.github.com>
2021-10-12 08:43:29 +0800

    Group per logic

--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -18,6 +18,116 @@ namespace GameOfLife
             var results = EvaluateGameOfLife(lifeMatrix, iteration);
         }
 
+        public static int[,] EvaluateGameOfLife(bool[,] lifeMatrix, int iteration)
+        {
+            var nbMaxtrix = new int[lifeMatrix.GetUpperBound(0) + 1, lifeMatrix.GetUpperBound(1) + 1];
+
+            int xLimit = lifeMatrix.GetUpperBound(0);
+            int yLimit = lifeMatrix.GetUpperBound(1);
+
+            for (int i = 0; i <= iteration; i++)
+            {
+                for (int xctr = 0; xctr <= xLimit; xctr++)
+                {
+                    for (int yctr = 0; yctr <= yLimit; yctr++)
+                    {
+                        var aliveNeigh = 0;
+
+                        aliveNeigh = CheckHorizontally(lifeMatrix, xctr, yctr, xLimit, yLimit, aliveNeigh);
+                        aliveNeigh = CheckVertically(lifeMatrix, xctr, yctr, yLimit, aliveNeigh);
+
+                        nbMaxtrix[xctr, yctr] = aliveNeigh;
+                    }
+                }
+
+                DisplayMatrices(i, lifeMatrix, nbMaxtrix);
+            }
+            return nbMaxtrix;
+        }
+
+        private static int[,] LifePerIteration(bool[,] lifeMatrix, int[,] nbMaxtrix)
+        {
+            int xLimit = nbMaxtrix.GetUpperBound(0);
+            int yLimit = nbMaxtrix.GetUpperBound(1);
+
+            for (int xctr = 0; xctr <= xLimit; xctr++)
+            {
+                for (int yctr = 0; yctr <= yLimit; yctr++)
+                {
+                    if (nbMaxtrix[xctr, yctr] == 1 || nbMaxtrix[xctr, yctr] == 0 || nbMaxtrix[xctr, yctr] >= 4)
+                    {
+                        lifeMatrix[xctr, yctr] = false;
+                    }
+                    else if (nbMaxtrix[xctr, yctr] == 3)
+                    {
+                        lifeMatrix[xctr, yctr] = true;
+                    }
+                }
+            }
+
+            return nbMaxtrix;
+        }
+
+
+        private static int CheckHorizontally(bool[,] lifeMatrix, int xctr, int yctr, int xLimit, int yLimit, int aliveNeigh)
+        {
+            if (xctr + 1 <= xLimit)
+            {
+                if (lifeMatrix[xctr + 1, yctr])
+                {
+                    aliveNeigh++;
+                }
+                aliveNeigh = CheckVertically(lifeMatrix, xctr + 1, yctr, yLimit, aliveNeigh);
+            }
+            if (xctr != 0)
+            {
+                if (lifeMatrix[xctr - 1, yctr])
+                {
+                    aliveNeigh++;
+                }
+                aliveNeigh = CheckVertically(lifeMatrix, xctr - 1, yctr, yLimit, aliveNeigh);
+            }
+
+            return aliveNeigh;
+        }
+
+        private static int CheckVertically(bool[,] lifeMatrix, int xctr, int yctr, int yLimit, int aliveNeigh)
+        {
+            if (yctr + 1 <= yLimit)
+            {
+                if (lifeMatrix[xctr, yctr + 1])
+                {
+                    aliveNeigh++;
+                }
+            }
+            if (yctr != 0)
+            {
+                if (lifeMatrix[xctr, yctr - 1])
+                {
+                    aliveNeigh++;
+                }
+            }
+
+            return aliveNeigh;
+        }
+
+        #region Display
+
+        private static void DisplayMatrices(int i, bool[,] lifeMatrix, int[,] nbMaxtrix)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Iteration: " + i);
+            Display(lifeMatrix);
+
+            LifePerIteration(lifeMatrix, nbMaxtrix);
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Neighbor count");
+            Display(nbMaxtrix);
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("--------------------------------------------------------------------", Console.ForegroundColor);
+        }
 
         private static void Display(bool[,] results)
         {
@@ -60,122 +170,6 @@ namespace GameOfLife
             }
             Console.WriteLine();
         }
-
-
-        public static int[,] EvaluateGameOfLife(bool[,] lifeMatrix, int iteration)
-        {
-            var nbMaxtrix = new int[lifeMatrix.GetUpperBound(0) + 1, lifeMatrix.GetUpperBound(1) + 1];
-
-            int xLimit = lifeMatrix.GetUpperBound(0);
-            int yLimit = lifeMatrix.GetUpperBound(1);
-
-            for (int i = 0; i <= iteration; i++)
-            {
-
-                for (int xctr = 0; xctr <= xLimit; xctr++)
-                {
-                    
-                    for (int yctr = 0; yctr <= yLimit; yctr++)
-                    {
-                        var aliveNeigh = 0;
-
-                        //Check horizontally
-                        if (xctr + 1 <= xLimit)
-                        {
-                            if (lifeMatrix[xctr + 1, yctr])
-                            {
-                                aliveNeigh++;
-                            }
-                            aliveNeigh = CheckVertically(lifeMatrix, xctr + 1, yctr, yLimit, aliveNeigh);
-                        }
-                        if (xctr != 0)
-                        {
-                            if (lifeMatrix[xctr - 1, yctr])
-                            {
-                                aliveNeigh++;
-                            }
-                            aliveNeigh = CheckVertically(lifeMatrix, xctr - 1, yctr, yLimit, aliveNeigh);
-                        }
-                            
-                            
-                        //Check vertically
-
-                        if (yctr + 1 <= yLimit)
-                        {
-                            if (lifeMatrix[xctr, yctr + 1])
-                            {
-                                aliveNeigh++;
-                            }
-                        }
-                        if (yctr != 0)
-                        {
-                            if (lifeMatrix[xctr, yctr - 1])
-                            {
-                                aliveNeigh++;
-                            }
-                        }
-
-                        nbMaxtrix[xctr, yctr] = aliveNeigh;
-                    }
-                }
-
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("Iteration: " + i);
-                Display(lifeMatrix);
-
-                LifePerIteration(lifeMatrix, nbMaxtrix);
-
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("Neighbor count");
-                Display(nbMaxtrix);
-
-                Console.ForegroundColor = ConsoleColor.Gray;
-                Console.WriteLine("--------------------------------------------------------------------", Console.ForegroundColor);
-            }
-            return nbMaxtrix;
-        }
-
-        private static int[,] LifePerIteration(bool[,] lifeMatrix, int[,] nbMaxtrix)
-        {
-            int xLimit = nbMaxtrix.GetUpperBound(0);
-            int yLimit = nbMaxtrix.GetUpperBound(1);
-
-            for (int xctr = 0; xctr <= xLimit; xctr++)
-            {
-                for (int yctr = 0; yctr <= yLimit; yctr++)
-                {
-                    if (nbMaxtrix[xctr, yctr] == 1 || nbMaxtrix[xctr, yctr] == 0 || nbMaxtrix[xctr, yctr] >= 4)
-                    {
-                        lifeMatrix[xctr, yctr] = false;
-                    }
-                    else if (nbMaxtrix[xctr, yctr] == 3)
-                    {
-                        lifeMatrix[xctr, yctr] = true;
-                    }
-                }
-            }
-
-            return nbMaxtrix;
-        }
-
-        private static int CheckVertically(bool[,] matrix, int xctr, int yctr, int yLimit, int aliveNeigh)
-        {
-            if (yctr + 1 <= yLimit)
-            {
-                if (matrix[xctr, yctr + 1])
-                {
-                    aliveNeigh++;
-                }
-            }
-            if (yctr != 0)
-            {
-                if (matrix[xctr, yctr - 1])
-                {
-                    aliveNeigh++;
-                }
-            }
-
-            return aliveNeigh;
-        }
+        #endregion Display
     }
 }
